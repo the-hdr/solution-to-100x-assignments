c5efb4e949b045517a832f562f71df8a82a35843
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2024-09-06 10:40:24 +0530

    add week-2 solution
    added week-2 solutions again after mistakenly deleting the whole repository

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -1,11 +1,15 @@
 /*
-  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
-  What's Anagram?
-  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+Write a function `isAnagram` which takes 2 parameters and returns true/false if one of them is the anagram of the other.
+What's an Anagram?
+- A string 's' is said to be an anagram of another string 't' if it is possible to rearrange the individual characters of 's' to form 't'. 
 */
 
-function isAnagram(str1, str2) {
-
+function isAnagram (str1, str2)
+{
+  const sortedCharStr1 = str1.toLowerCase().split('').sort().join('');
+  const sortedCharStr2 = str2.toLowerCase().split('').sort().join('');
+  
+  return sortedCharStr1 == sortedCharStr2;
 }
 
 module.exports = isAnagram;
--- week-2/week-2-js/easy/expenditure-analysis.js
+++ week-2/week-2-js/easy/expenditure-analysis.js
@@ -3,7 +3,7 @@
   and return a list of objects where each object is unique category-wise and has total price spent as its value.
   transactions is an array where each
   Transaction - an object like 
-        {
+  {
 		id: 1,
 		timestamp: 1656076800000,
 		price: 10,
@@ -13,8 +13,34 @@
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+function calculateTotalSpentByCategory (transactions)
+{
+	const totalNumberOfTransactions = transactions.length;
+
+	let totalSpentByCategory = [];
+	let categoryToIndexMap = new Map();
+	let currentTotalNumberOfCategories = 0;
+	for (let i = 0; i < totalNumberOfTransactions; ++i)
+	{
+		if (false == categoryToIndexMap.has(transactions[i].category))
+		{
+			const newCategoryIndex = currentTotalNumberOfCategories;
+			++currentTotalNumberOfCategories;
+			
+			const currentTransactionCategory = transactions[i].category;                                                                                     xxx
+
+			totalSpentByCategory.push ({
+				category: currentTransactionCategory,
+				totalSpent: 0
+			});
+			categoryToIndexMap.set (currentTransactionCategory, newCategoryIndex);
+		}
+
+		const currentCategoryIndex = categoryToIndexMap.get(transactions[i].category);
+		totalSpentByCategory[currentCategoryIndex].totalSpent += transactions[i].price;
+	}
+
+	return totalSpentByCategory;
 }
 
 module.exports = calculateTotalSpentByCategory;
