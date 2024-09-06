1adec4986cefff27aa8aebc1b602d797032ea750
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2024-09-06 11:45:01 +0530

    update palindrome.js
    added solution to palindrome.js

--- week-2/week-2-js/medium/palindrome.js
+++ week-2/week-2-js/medium/palindrome.js
@@ -3,8 +3,44 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
-function isPalindrome(str) {
-  return true;
+function isAlphabet (inputCharacter)
+{
+    return /^[a-zA-Z]$/.test(inputCharacter);
+}
+
+function isPalindrome (inputString)
+{
+    const inputStringSize = inputString.length;
+    let frontCounter = 0, backCounter = inputStringSize - 1;
+    
+    while (frontCounter < backCounter)
+    {
+        const frontCharacter = inputString[frontCounter].toLowerCase();
+        const backCharacter = inputString[backCounter].toLowerCase();
+
+        if (isAlphabet (frontCharacter) && isAlphabet (backCharacter))
+        {
+            if (frontCharacter != backCharacter)
+            {
+                return false;
+            }
+
+            ++frontCounter;
+            --backCounter;
+        }
+        else
+        {
+            if (false == isAlphabet (frontCharacter))
+            {
+                ++frontCounter;
+            }
+            if (false == isAlphabet (backCharacter))
+            {
+                --backCounter;
+            }
+        }
+    }
+    return true;
 }
 
 module.exports = isPalindrome;
