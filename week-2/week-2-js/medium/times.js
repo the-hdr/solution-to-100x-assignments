54c0eb8afbfa5cefd60056586af9a4b755787b73
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2024-09-06 11:46:01 +0530

    update times.js

--- week-2/week-2-js/medium/times.js
+++ week-2/week-2-js/medium/times.js
@@ -8,6 +8,7 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
-function calculateTime(n) {
-    return 0.01;
+function calculateTime (n)
+{
+    
 }
