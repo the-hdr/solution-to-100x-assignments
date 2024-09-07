56df342f6c8db3bd604da646dcef069366f5858e
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2024-09-07 07:06:16 +0530

    update todo-list.js
    add solution to todo-list.js

--- week-2/week-2-js/hard/todo-list.js
+++ week-2/week-2-js/hard/todo-list.js
@@ -10,8 +10,66 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Todo {
+class Todo
+{
+    constructor()
+    {
+        this.todoList = [];
+    }
 
+    add(newTask)
+    {
+        this.todoList.push (newTask);
+    }
+
+    remove(indexOfTaskToBeDeleted)
+    {
+        if (indexOfTaskToBeDeleted >= 0 && indexOfTaskToBeDeleted < this.todoList.length)
+        {
+            this.todoList.splice (indexOfTaskToBeDeleted, 1);
+        }
+        else
+        {
+            console.log ("remove(): INDEX OUT OF BOUND!");
+            return null;
+        }
+    }
+
+    update (updationIndex, updatedTodo)
+    {
+        if (updationIndex >= 0 && updationIndex < this.todoList.length)
+        {
+            this.todoList[updationIndex] = updatedTodo;
+        }
+        else
+        {
+            console.log ("update(): INDEX OUT OF BOUND!");
+            return null;
+        }
+    }
+
+    getAll()
+    {
+        return this.todoList;
+    }
+
+    get(indexOfTodo)
+    {
+        if (indexOfTodo >= 0 && indexOfTodo < this.todoList.length)
+        {
+            return this.todoList[indexOfTodo];
+        }
+        else
+        {
+            console.log ("get(): INDEX OUT OF BOUND!");
+            return null;
+        }
+    }
+
+    clear()
+    {
+        this.todoList = [];
+    }
 }
 
 module.exports = Todo;
