aa874b45bb193c302f144bb5bbe587aa2078d3b2
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2024-09-08 18:07:46 +0530

    update 1-counter.js
    slight improvement enhancement lol

--- week-2/week-2-async-js/easy/1-counter.js
+++ week-2/week-2-async-js/easy/1-counter.js
@@ -1,11 +1,13 @@
 const second = 1000;
+const goBackPreviousLineString = '\x1b[F';
+
 let currentTime = 1;
 
 function printCurrentTime ()
 {
-    console.clear();
     console.log (currentTime);
-    currentTime++;
+    ++currentTime;
+    process.stdout.write (goBackPreviousLineString);
 }
 
 setInterval (printCurrentTime, 1 * second);
