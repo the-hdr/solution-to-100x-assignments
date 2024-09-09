392ab6d58f7a353caf11c9794cb36175767a59c3
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2024-09-09 23:41:33 +0530

    update 1-file-cleaner.js

--- week-2/week-2-async-js/medium/1-file-cleaner.js
+++ week-2/week-2-async-js/medium/1-file-cleaner.js
@@ -1,4 +1,18 @@
 const fs = require('fs');
 
 const data = fs.readFileSync('example.txt', 'utf8');
+
+// printing data just for fun lol:
 console.log(data);
+
+// sanitizing data:
+const sanitizedData = data.split(/\s+/).join(' ').trim();
+
+fs.writeFile ('example.txt', sanitizedData,
+    (err) =>
+    {
+        if (err) throw err;
+        console.log('File written successfully.');
+    });  
+
+console.log (sanitizedData);
