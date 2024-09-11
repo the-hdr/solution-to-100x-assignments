d0382623e6c0df0e6e15a62455832b5e3622824d
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2024-09-11 18:04:48 +0530

    update script.js

--- week-3/easy/quiz-app/script.js
+++ week-3/easy/quiz-app/script.js
@@ -4,30 +4,47 @@ let currentScore = 0;
 let currentQuestionNumber = 0;
 
 let currentCorrectOption = '?';
-let currentSelectedOption = '?';
 
-const totalQuestions = quizData.length;
+const totalNumberOfQuestions = quizData.length;
 
 function updateScore()
 {
-    if (currentCorrectOption == currentSelectedOption)
-    {
-        ++score;
-    }
+    currentCorrectOption = quizData[currentQuestionNumber].correct;
+    const correctRadioButton = document.getElementById(currentCorrectOption);
 
-    displayQuestion (currentQuestionNumber);
+    if (correctRadioButton.checked)
+    {
+        ++currentScore;
+    }
+    
+    ++currentQuestionNumber;
+    console.log (currentQuestionNumber);
+    displayQuestion ();
 }
 
-function displayQuestion ()
+function displayQuestion()
 {
-    if (currentQuestionNumber >= totalQuestions)
+    if (currentQuestionNumber >= totalNumberOfQuestions)
     {
-        return;
+        const scoreString = 'Score: ' + currentScore.toString() + '/' + totalNumberOfQuestions.toString();
+        alert (scoreString);
     }
 
     const questionDiv = document.querySelector('.question');
+    let optionBoxes = document.querySelectorAll('.option');
 
-    questionDiv.innerText = quizData[currentQuestionNumber].question;
+    // set question:
+    questionDiv.innerHTML = quizData[currentQuestionNumber].question;
+
+    // set options:
+    console.log (optionBoxes);
+    optionBoxes[0].innerHTML = quizData[currentQuestionNumber].a;
+    optionBoxes[1].innerHTML = quizData[currentQuestionNumber].b;
+    optionBoxes[2].innerHTML = quizData[currentQuestionNumber].c;
+    optionBoxes[3].innerHTML = quizData[currentQuestionNumber].d;
 }
+
+const submitButton = document.querySelector('.submit-button');
+submitButton.addEventListener('click', updateScore);
 
 displayQuestion ();
