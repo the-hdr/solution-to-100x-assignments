e183a3352dfe21ffa4d40e50006fb8d9a9ec489e
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2024-09-11 11:31:00 +0530

    update script.js
    remove naan-sainse!

--- week-3/easy/bg-color-changer/script.js
+++ week-3/easy/bg-color-changer/script.js
@@ -1,13 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Get all buttons with the class 'changer-button'
+document.addEventListener('DOMContentLoaded', () =>
+{
     const buttons = document.querySelectorAll('.changer-button');
 
-    // Add a click event listener to each button
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            // Get the color from the data-color attribute
+    buttons.forEach(button =>
+    {
+        button.addEventListener('click', () =>
+        {
             const color = button.getAttribute('color-data');
-            // Change the background color of the body
             document.body.style.backgroundColor = color;
         });
     });
