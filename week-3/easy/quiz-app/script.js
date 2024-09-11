9165e0d40fadf58083c9aea5ca98c79480d59f1d
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2024-09-11 12:04:54 +0530

    update script.js

---
+++ week-3/easy/quiz-app/script.js
@@ -0,0 +1,9 @@
+import { quizData } from './quizData.js'; 
+
+let currentScore = 0;
+
+const totalQuizItems = quizData.length;
+for (let questionNumber = 0; questionNumber < totalQuizItems; ++i)
+{
+    
+}
